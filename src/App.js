aef9aef902afa0efa67db44deeaa65e128e7fe79
Takamichi Omori <merrytakamerry@gmail.com>
2019-04-23 21:19:39 +0900

    クイズ作成画面を追加(文章のみ)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { firebase } from './firebase'
+
 import Login from './login'
-import UserData from './UserData'
+
+import FormApp from './form'
 import './App.css'
 
 class App extends Component {
@@ -26,8 +28,9 @@ class App extends Component {
       <div className="App">
         {this.state.user ? (
           <div>
-            <UserData user={this.state.user}></UserData>
+            { /* <UserData user={this.state.user}></UserData> */}
             <button onClick={this.logout}>Google Logout</button>
+            <FormApp uid={this.state.user.l} />
           </div>
         ) : (
             <div>
