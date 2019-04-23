4401c0217fbc9bcdac6083d48c1a47cea49cc74e
Takamichi Omori <merrytakamerry@gmail.com>
2019-04-23 21:41:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { firebase } from './firebase'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Login from './login'
 import UserData from './UserData'
 import './App.css'
@@ -23,20 +24,33 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        {this.state.user ? (
-          <div>
-            <UserData user={this.state.user}></UserData>
-            <button onClick={this.logout}>Google Logout</button>
-          </div>
-        ) : (
-            <div>
-              <Login user={this.state.user} changeUserState={this.changeUserState}></Login>
-            </div>
-          )}
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/friends' component={Friends} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+)
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+)
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <p>ここにフレンズのリストを書きます</p>
+  </div>
+)
 export default App
