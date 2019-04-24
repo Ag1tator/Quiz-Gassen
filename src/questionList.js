65f1aaf159a2626b5d6e721f8ce42ee087a68f31
Takamichi Omori <merrytakamerry@gmail.com>
2019-04-24 19:04:03 +0900

    とりあえず１個はでた

--- src/questionList.js
+++ src/questionList.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { getQuestions } from './firebase'
+import Accordion from './Accordion'
+import AccordionList from './AccordionList'
+
+
 class QuestionList extends React.Component {
-
-
-  getQuestionsFromFirestore() {
-    const questions = getQuestions()
-    console.log(questions)
+  constructor(props) {
+    super(props)
+    this.state = {
+      questions: null,
+    }
+    this.getQuestionsFromFirebase()
+  }
+  getQuestionsFromFirebase = () => {
+    getQuestions().then(snap => {
+      this.setState({ questions: snap })
+      console.log(this.state)
+    }).catch(err => {
+      console.log("Failed get Questions")
+    })
   }
   render() {
-    this.getQuestionsFromFirestore()
+    let a = [];
+    if (this.state.questions !== null) {
+      this.state.questions.forEach(doc => {
+        if (doc.exists) {
+          console.log("document data", doc.data());
+          a.push(<div>{doc.data().body}</div>)
+        } else {
+          console.log("no document found");
+        }
+      })
+    }
     return (
-      <div>question List</div>
-    )
+      <div className="container">
+        {this.state.questions ? a[0] : <div></div>}
+      </div>
+    );
   }
 }
-
 export default QuestionList
