45a6b2365191cc72d7647febecd0e50b2fc4ce8e
sasa-kou <sasakou11428@gmail.com>
2019-04-26 15:53:10 +0900

    Update Home.js
    Homeにログイン機能を作成

--- src/design/components/Home/Home.js
+++ src/design/components/Home/Home.js
@@ -3,12 +3,22 @@ import React, { Component } from 'react'
 import './../style.scss'
 
 class Home extends Component {
+  constructor(props){
+      super(props)
+  }
+
     render() {
         return (
             <div className="fullScreen">
                 <div className="loginForm">
                     <h1>Title</h1>
-                    <button className="primaryButton">ログイン</button>
+                    {this.props.user
+                      ? <button onClick={this.props.logout} className="primaryButton">Google Logout</button>
+                      : <button onClick={this.props.login} className="primaryButton">Google Login</button>
+                    }
+                    {
+                      //<button className="primaryButton">ログイン</button>
+                    }
                 </div>
             </div>
         )
