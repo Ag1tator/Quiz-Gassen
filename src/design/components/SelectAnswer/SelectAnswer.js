20c0fbefce21c02c0377338cd3a637b70f358a49
sasa-kou <sasakou11428@gmail.com>
2019-04-26 14:51:35 +0900

    Update SelectAnswer.js
    コンパクトじゃない実装

--- src/design/components/SelectAnswer/SelectAnswer.js
+++ src/design/components/SelectAnswer/SelectAnswer.js
@@ -3,38 +3,68 @@ import React, { Component } from 'react'
 import './../style.scss'
 
 class Answer extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      fullScreen: false,
-      checkbutton: false
-    }
+  state = {
+    changeScreen: false,
+    checkbutton0: false,
+    checkbutton1: false,
+    checkbutton2: false,
+    checkbutton3: false,
   }
 
-  changeButtonState = () => {
+  changeButton0State = () => {
     this.setState({
-      checkbutton: true
+      changeScreen: true,
+      checkbutton1: true,
+      checkbutton2: true,
+      checkbutton3: true,
+    })
+  }
+
+  changeButton1State = () => {
+    this.setState({
+      changeScreen: true,
+      checkbutton0: true,
+      checkbutton2: true,
+      checkbutton3: true,
+    })
+  }
+
+  changeButton2State = () => {
+    this.setState({
+      changeScreen: true,
+      checkbutton0: true,
+      checkbutton1: true,
+      checkbutton3: true,
+    })
+  }
+
+  changeButton3State = () => {
+    this.setState({
+      changeScreen: true,
+      checkbutton0: true,
+      checkbutton1: true,
+      checkbutton2: true,
     })
   }
 
     render() {
         return (
-            <div className={this.state.fullScreen ? "fullScreen": "fullScreen backGroundGray"}>
+            <div className={this.state.changeScreen ? "fullScreen backGroundGray": "fullScreen"}>
                 <div className="quizForm">
                     <div className="quizTopRapper">
                         <div className="quizTop">
-                            <h1>自分の答えを<br />選択！</h1>
-                            <div className="loaderRapper none">
+                            <h1 className={this.state.changeScreen ? "none": ""}>自分の答えを<br />選択！</h1>
+                            <div className={this.state.changeScreen ? "loaderRapper": "loaderRapper none"}>
                                 <h1>正解発表待ち</h1>
                                 <div className="loader">Loading...</div>
                             </div>
                         </div>
                     </div>
                     <ul className="quizList">
-                        <li className={this.state.checkbutton ? "hidden": ""}><button onClick={this.changeButtonState}>ソーダ</button></li>
-                        <li className={this.state.checkbutton ? "hidden": ""}><button onClick={this.changeButtonState}>カルピス</button></li>
-                        <li className={this.state.checkbutton ? "hidden": ""}><button onClick={this.changeButtonState}>R1</button></li>
-                        <li className={this.state.checkbutton ? "hidden": ""}><button onClick={this.changeButtonState}>オレンジジュース</button></li>
+                        <li className={this.state.checkbutton0 ? "hidden": ""}><button onClick={this.changeButton0State}>ソーダ</button></li>
+                        <li className={this.state.checkbutton1 ? "hidden": ""}><button onClick={this.changeButton1State}>カルピス</button></li>
+                        <li className={this.state.checkbutton2 ? "hidden": ""}><button onClick={this.changeButton2State}>R1</button></li>
+                        <li className={this.state.checkbutton3 ? "hidden": ""}><button onClick={this.changeButton3State}>オレンジジュース</button></li>
                     </ul>
                 </div>
             </div>
