e672c80a88c363c0b4ababd07486d497c63bdaaa
Takamichi Omori <merrytakamerry@gmail.com>
2019-04-29 10:55:45 +0900

    正解発表後のローディングを追加

--- src/design/components/Answer/Answer.js
+++ src/design/components/Answer/Answer.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 import './../style.scss'
+import Loading from '../Loading/Loading';
 
 class Answer extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            onLoading: false
+        }
+    }
+    loadingSwitchHandler = () => {
+        this.setState({
+            onLoading: !this.state.onLoading
+        })
+    }
     render() {
         let answer = null;
         if (this.props.isCollect) {
@@ -11,24 +22,31 @@ class Answer extends Component {
         } else {
             answer = <div className="wrong">不正解</div>
         }
-        return (
-            <div className="fullScreen">
-                <div className="answerRapper">
-                    <div className="answer">
-                        <img src={this.props.image} alt="hogehoge"></img>
-                        {answer}
+        if (this.state.onLoading) {
+            return (
+                <Loading />
+            )
+        } else {
+            return (
+                <div className="fullScreen">
+                    <div className="answerRapper">
+                        <div className="answer">
+                            <img src={this.props.image} alt="hogehoge"></img>
+                            {answer}
+                        </div>
+                    </div>
+                    <div className="commentary">
+                        <h1>{this.props.answer}</h1>
+                        <div className="description">
+                            {this.props.description}
+                        </div>
+                        <button className="primaryButton" onClick={this.loadingSwitchHandler}>次へ</button>
                     </div>
                 </div>
-                <div className="commentary">
-                    <h1>{this.props.answer}</h1>
-                    <div className="description">
-                        {this.props.description}
-                    </div>
-                    <button className="primaryButton">次へ</button>
-                </div>
-            </div>
-        )
+            )
+        }
     }
+
 }
 
 export default Answer
