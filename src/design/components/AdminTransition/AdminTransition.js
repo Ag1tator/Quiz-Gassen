88945f9ffa4bc0f1aac56ea7b13541d52fab8f53
Takamichi Omori <merrytakamerry@gmail.com>
2019-04-29 17:39:29 +0900

    画面遷移が正常に行われるようになった

--- src/design/components/AdminTransition/AdminTransition.js
+++ src/design/components/AdminTransition/AdminTransition.js
@@ -23,36 +23,43 @@ class AdminTransition extends Component {
     onClick = () => {
         console.log("clicked!", this.state.roomData.currentQuizNum + 1, this.state.roomData.quiz.length)
         let currentData = this.state.roomData
-        if (this.state.roomData.currentQuizNum + 1 === this.state.roomData.quiz.length) { // おわったとき
-            alert("しゅ〜りょ〜")
-            currentData.isResult = true;
-        } else {
-            if (this.state.roomData.isCheckAnswer) {
-                currentData.isCheckAnswer = false;
-                currentData.isShowImage = false;
-                currentData.currentQuizNum++;
-            } else if (this.state.roomData.isShowImage) {
-                currentData.isCheckAnswer = true;
-            } else if (this.state.roomData.isSelectResolution) {
-                currentData.isShowImage = true;
+
+        if (this.state.roomData.isCheckAnswer) {
+            currentData.isCheckAnswer = false;
+            currentData.isShowImage = false;
+            currentData.isSelectResolution = false;
+            currentData.currentQuizNum++;
+            if (this.state.roomData.currentQuizNum === this.state.roomData.quiz.length) { // おわったとき
+                alert("しゅ〜りょ〜")
+                currentData.isResult = true;
             }
+        } else if (this.state.roomData.isShowImage) {
+            currentData.isCheckAnswer = true;
+        } else if (this.state.roomData.isSelectResolution) {
+            currentData.isShowImage = true;
+        } else if (this.state.roomData.isWaiting) {
+            currentData.isSelectResolution = true
         }
+
         firestore.collection('room').doc(this.state.roomName).set(currentData)
 
     }
     getCurrentButton = () => {
         //isSelectResolution => isShowImage => isCheckAnswer => isResult
         console.log(this.state.roomData)
-        if (this.state.roomData.isResult) {
-            return <div>結果を表示する</div>
-        } else if (this.state.roomData.isCheckAnswer) {
-            return <div>答えを表示する</div>
+        if (this.state.roomData.isCheckAnswer) {
+            if (this.state.roomData.currentQuizNum + 1 === this.state.roomData.quiz.length) { // おわったとき
+                return <div>最終結果を表示する</div>
+            }
+            return <div>次の問題に進む</div>
         } else if (this.state.roomData.isShowImage) {
-            return <div>{this.state.roomData.currentQuizNum}問目を出題する</div>
-        } else {//(this.roomData.isSelectResolution)
-            return <div>{this.state.roomData.currentQuizNum}問目の解像度選択を始める</div>
-        }
+            return <div>答えを表示する</div>
+        } else if (this.state.roomData.isSelectResolution) {
+            return <div>{this.state.roomData.currentQuizNum + 1}問目を出題する</div>
+        } else if (this.state.roomData.isWaiting)
+            return <div>{this.state.roomData.currentQuizNum + 1}問目の解像度選択を始める</div>
     }
+
     render() {
         if (this.state.roomData) {
             return (
